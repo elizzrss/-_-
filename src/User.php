9643843246d815a55e6cadd4a89a009375b07b47
elizzrss <korolkova.elizaveta4@gmail.com>
2025-01-07 21:30:28 +0500

    less5

--- src/User.php
+++ src/User.php
@@ -1,10 +1,29 @@
 <?php
+namespace App;
 
 class User
 {
-    public int $id;
+    public function __construct(
+        private UUID $uuid,
+        private UUID $authorUuid,
+        private string $nickname,
+        private string $firstName,
+        private string $lastName
+    ) {
+    }
 
-    public string $firstName;
+    public function getUuid(): UUID{
+        return $this->uuid;}
 
-    public string $lastName;
+    public function getAuthorUuid(): UUID{
+        return $this->authorUuid;}
+
+    public function getNickname(): string{
+        return $this->nickname;}
+
+    public function getFirstName(): string{
+        return $this->firstName;}
+
+    public function getLastName(): string{
+        return $this->lastName;}
 }
